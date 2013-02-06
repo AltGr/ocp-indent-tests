e73bcb92ccc2c70ed4b6aff9c0a2d66cd972d3b6
Louis Gesbert <louis.gesbert@ocamlpro.com>
2013-02-06 09:37:44 +0100

    Added source xen-api

---
+++ current/xen-api/xen-api-master/ocaml/util/version.mli
@@ -0,0 +1,12 @@
+val git_id : string
+val hostname : string
+val date : string
+val product_version : string
+val product_version_text : string
+val product_version_text_short : string
+val platform_name: string
+val platform_version: string
+val product_brand : string
+val build_number : string
+val xapi_version_major : int
+val xapi_version_minor : int
